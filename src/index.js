2864436e44910ebae86867ac477a6f6974827086
JackyTang <jackytang01@gmail.com>
2024-06-27 17:30:51 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const routes = {
   "ecr.juyou.live": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.juyou.live": dockerHub,
 };
 
 function routeByHosts(host) {
